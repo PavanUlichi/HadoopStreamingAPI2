c5021e293fed3153f27c4c57aab4be06922803a7
ulichitr <pavan.ulichi@gmail.com>
2016-12-08 20:17:13 -0500

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -2,8 +2,11 @@
 
 import sys
 import ast
+#executes till there is an input line from STDIN
 for line in sys.stdin:
+        #taking each line to a variable one by one
         line = line.strip()
+        #taking each word in the line, separated by tab space to a variable "words"
         words=line.split('\t')
         data = ast.literal_eval(words[04])
         try:
